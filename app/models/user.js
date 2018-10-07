95231530299835c1f01ce1a1016f7f649e375b43
Shreyas <shreyas.s104@gmail.com>
2018-10-07 16:15:39 +0530

    This contains adding of UserSchema for User Model and also using validator functionality

--- app/models/user.js
+++ app/models/user.js
@@ -1,19 +1,44 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-var User = mongoose.model('User',{
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true
+        required: true,
+        minlength: 8,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: 'Email {VALUE}  should be of proper valid format'
+        }
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minlength: 6,
+        validate: {
+            validator: function(val){
+                return validator.isAlphanumeric(val,'en-US');
+            },
+            message: 'Password {VALUE} must be of Alpha Numeric format'
+        }
     },
     name: {
         type: String,
         required: true
-    }
-
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+var User = mongoose.model('User',UserSchema);
 
 module.exports= {User};
